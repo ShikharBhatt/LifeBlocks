4e851bbc069448c5cf2c11bed68fc5519ef9da47
Sumitjh26997 <sumit.hotchandani@gmail.com>
2019-01-09 11:15:28 +0530

    Initialize Routes and Firebase

--- aadhaar/src/components/App/index.js
+++ aadhaar/src/components/App/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router , Route } from 'react-router-dom';
+import Navigation from '../Navigation';
+import LandingPage from "../Landing";
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+
+import * as ROUTES from '../../constants/routes';
+
 
 const App = () => (
+  <Router>
   <div>
-    <h1>App</h1>
+      <Navigation />
+      <hr />
+
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
   </div>
+  </Router>
 );
 
 export default App;
