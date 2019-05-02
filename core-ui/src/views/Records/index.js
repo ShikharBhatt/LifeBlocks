10d2a832ad9ddddef2947e45efe382f835395ba7
Shikhar Bhatt <shikhar.bhatt011@gmail.com>
2019-05-03 00:07:01 +0530

    Add: Revoke Records

--- core-ui/src/views/Records/index.js
+++ core-ui/src/views/Records/index.js
@@ -2,6 +2,7 @@ import ViewRecords from './ViewRecords';
 import ShareRecords from './ShareRecords';
 import ApplyPolicy from './ApplyPolicy';
 import ViewPolicy from './ViewPolicy';
+import RevokeAccess from './RevokeAccess';
 export {
-  ViewRecords, ShareRecords, ApplyPolicy, ViewPolicy
+  ViewRecords, ShareRecords, ApplyPolicy, ViewPolicy, RevokeAccess
 };
