64e35e609e1a14e816ebc7a0b19f4f8fc0a2ebaa
Sumitjh26997 <sumit.hotchandani@gmail.com>
2019-05-07 19:42:14 +0530

    Successful MySQL entry using API

--- transaction_api/app.js
+++ transaction_api/app.js
@@ -4,7 +4,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const port = 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/hello', (req, res) => {
+    res.send({ express: 'Hello From Express' })
+})
+
+app.post('/api/insert', async (req, res) => {
+    console.log(req.body.orgAddress,req.body.contractAddress)
+    let insertTask = await task.insert(req.body.orgAddress,req.body.contractAddress);
+    console.log("Inserted Task : ", insertTask);
+    res.send(
+        `I received your POST request. This is what you sent me: ${req.body.orgAddress},${req.body.contractAddress}`,
+      );
+})
 // async () => {
 //     let insertTask = await task.insert("send ethers");
 //     console.log("Inserted Task : ", insertTask);
@@ -19,4 +35,4 @@ const app = express();
 //     console.log("Task deleted ? : ", deleteTask);
 // }
 
-app();
+app.listen(port, () => console.log(`Listening on port ${port}`));
