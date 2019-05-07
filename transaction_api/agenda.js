37e388719a2453226ff86717d1a3d645dbdf6490
Sumitjh26997 <sumit.hotchandani@gmail.com>
2019-05-07 21:33:50 +0530

    Agenda not working in listener

---
+++ transaction_api/agenda.js
@@ -0,0 +1,80 @@
+const Web3 = require('web3');
+const web3 = new Web3("http://127.0.0.1:7545");
+const Tx = require("ethereumjs-tx");
+
+const user = '0x28bfC651f2F8396350daE7E3883952a7F053E828';
+const u_priv = Buffer('fd6380f39c55ffa634f556c723884d3f3083811f494fffa8095f99fb3d23c999','hex');
+const abi = [{"constant":false,"inputs":[{"name":"_premium","type":"uint256"}],"name":"setPremium","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getState","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_reason","type":"string"}],"name":"requestRecords","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"recordID","type":"uint256"},{"name":"_masterkey","type":"string"}],"name":"getRecordsRenewal","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"policyGrace","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_coverage","type":"uint256"}],"name":"renewPolicy","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"confirmRenewal","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[],"name":"applyPolicy","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"recordID","type":"uint256"},{"name":"_masterkey","type":"string"}],"name":"getRecordsApplied","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getRecords","outputs":[{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renewPolicy","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"state","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getPremium","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"policyLapse","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"confirmPolicy","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[],"name":"policyInactive","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getDetails","outputs":[{"name":"","type":"address"},{"name":"","type":"address"},{"name":"","type":"uint256"},{"name":"","type":"uint8"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"string"},{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"prevState","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"contractBuyer","type":"address"},{"name":"contractSeller","type":"address"},{"name":"_coverage","type":"uint256"}],"payable":true,"stateMutability":"payable","type":"constructor"}];
+
+const recv = '0xa577C9d2BA3bB4aD1e09df6dFf697Cb3A639eB7a';
+
+const Agenda = require('agenda');
+
+async function getTransactionCount(data){
+  let raw;
+  const contract = data.contract
+  const worker = data.worker
+  console.log("Inside: ",contract,worker)
+  //await web3.eth.getBlockNumber().then(console.log);
+  await web3.eth.getTransactionCount(worker, async (err, txCount) => {
+    var data = new web3.eth.Contract(abi, contract);
+    var x = data.methods.applyPolicy().encodeABI();
+    console.log("ABI:",x)
+    const txObject = {
+      nonce : web3.utils.toHex(txCount),
+      to : contract,   
+      gasLimit : web3.utils.toHex(4700000),
+      gasPrice : web3.utils.toHex(web3.utils.toWei('0','gwei')),
+      data : x,
+    }
+
+    //Sign a transaction
+  const tx = new Tx(txObject);
+  tx.sign(u_priv);
+
+  const serializedTransaction = tx.serialize();
+  raw = "0x" + serializedTransaction.toString("hex");
+  console.log("raw:",raw);
+  //Broadcast a transaction
+  });
+  await web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+    if(err)
+      console.log(err)
+    else
+      console.log("txHash : ", txHash);
+    });
+  // return count;
+}
+
+async function run() {
+  //const db = await MongoClient.connect('mongodb://localhost:27017/agendatest');
+
+  // Agenda will use the given mongodb connection to persist data, so jobs
+  // will go in the "agendatest" database's "jobs" collection.
+  const agenda = new Agenda({db:{address:'mongodb://localhost:27017/agendatest'}});
+
+  // Define a "job", an arbitrary function that agenda can execute
+  agenda.define('transact', async (job,done) => {
+    console.log('transact : in job');
+    console.log(job.attrs.data.contract);
+    await getTransactionCount(job.attrs.data, () => {
+      console.log("done")
+    })
+    process.exit(0);
+  });
+
+  // Wait for agenda to connect. Should never fail since connection failures
+  // should happen in the `await MongoClient.connect()` call.
+  await new Promise(resolve => agenda.once('ready', resolve));
+
+  // Schedule a job for 1 second from now and persist it to mongodb.
+  // Jobs are uniquely defined by their name, in this case "hello"
+
+  agenda.schedule(new Date(Date.now() + 20000), 'transact',{contract : recv, worker : user});
+  agenda.start();
+}
+
+run().catch(error => {
+  console.error(error);
+  process.exit(-1);
+});
--- transaction_api/listener.js
+++ transaction_api/listener.js
@@ -1,5 +1,9 @@
 const mySqlEvents = require('mysql-events');
 var Agenda = require('agenda');
+const Web3 = require('web3');
+const web3 = new Web3("http://127.0.0.1:7545");
+const Tx = require("ethereumjs-tx");
+const u_priv = Buffer('9c6f137a9c254ae3fa844d877f62cab6bd863ddccedc2cb7912c9a096ff1f210','hex');
 
 
 var agenda = new Agenda({db: {address: 'localhost:27017/agenda-test'}});
@@ -9,17 +13,74 @@ var dbConfig = {
     host : "localhost",
 }
 
+async function getTransactionCount(data){
+    let raw;
+    const contract = data.contract
+    const worker = data.worker
+    console.log("Inside: ",contract,worker)
+    //await web3.eth.getBlockNumber().then(console.log);
+    await web3.eth.getTransactionCount(worker, async (err, txCount) => {
+      var data = new web3.eth.Contract(abi, contract);
+      var x = data.methods.applyPolicy().encodeABI();
+      console.log("ABI:",x)
+      const txObject = {
+        nonce : web3.utils.toHex(txCount),
+        to : contract,   
+        gasLimit : web3.utils.toHex(4700000),
+        gasPrice : web3.utils.toHex(web3.utils.toWei('0','gwei')),
+        data : x,
+      }
+  
+      //Sign a transaction
+    const tx = new Tx(txObject);
+    tx.sign(u_priv);
+  
+    const serializedTransaction = tx.serialize();
+    raw = "0x" + serializedTransaction.toString("hex");
+    console.log("raw:",raw);
+    //Broadcast a transaction
+    });
+    await web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+      if(err)
+        console.log(err)
+      else
+        console.log("txHash : ", txHash);
+      });
+    // return count;
+  }
+
 var connection = mySqlEvents(dbConfig);
 
 var listener = connection.add(
-    'test.tasks',
-    function(oldRow, newRow, event){
+    'agenda_tasks.tasks',
+    async function(oldRow, newRow, event){
         if(oldRow === null){
-            console.log("insert : ",event.rows);
+            //console.log("insert : ",event.rows);
             taskobj = event.rows
-            if(taskobj[0].task == "send ethers"){
-                console.log(taskobj[0].task_id,taskobj[0].date)
+            
+            if(taskobj[0].task == "DEFAULT_TASK"){
                 
+                console.log(taskobj[0].task_id,taskobj[0].date,taskobj[0].org_address,taskobj[0].contract_address)
+                
+                // const job_name = `transact${taskobj[0].contract_address}`;
+                // console.log(job_name)
+                
+                agenda.define('transact', async (job,done) => {
+                    console.log('transact : in job');
+                    console.log(job.attrs.data.contract);
+                    await getTransactionCount(job.attrs.data, () => {
+                      console.log("done")
+                    })
+                    process.exit(0);
+                  });
+                  
+                await new Promise(resolve => agenda.once('ready', resolve));
+
+                  // Schedule a job for 1 second from now and persist it to mongodb.
+                  // Jobs are uniquely defined by their name, in this case "hello"
+                console.log("agenda")
+                agenda.schedule(new Date(Date.now() + 20000),'transact',{contract : taskobj[0].contract_address, worker : taskobj[0].org_address});
+                agenda.start();
             }
             else
                 console.log("false")
