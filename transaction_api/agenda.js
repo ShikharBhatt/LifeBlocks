9f9460ebf996c0368d6061c47d1a3aec93d614db
Sumitjh26997 <sumit.hotchandani@gmail.com>
2019-05-08 09:43:04 +0530

    Transaction scheduler working

--- transaction_api/agenda.js
+++ transaction_api/agenda.js
@@ -2,11 +2,11 @@ const Web3 = require('web3');
 const web3 = new Web3("http://127.0.0.1:7545");
 const Tx = require("ethereumjs-tx");
 
-const user = '0x28bfC651f2F8396350daE7E3883952a7F053E828';
-const u_priv = Buffer('fd6380f39c55ffa634f556c723884d3f3083811f494fffa8095f99fb3d23c999','hex');
+//const user = '0x28bfC651f2F8396350daE7E3883952a7F053E828';
+const u_priv = Buffer('0079a1cd9d342a4f92d14973f38a4a4d6ba1ada81d57831b7818efce5978cabd','hex');
 const abi = [{"constant":false,"inputs":[{"name":"_premium","type":"uint256"}],"name":"setPremium","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getState","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_reason","type":"string"}],"name":"requestRecords","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"recordID","type":"uint256"},{"name":"_masterkey","type":"string"}],"name":"getRecordsRenewal","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"policyGrace","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_coverage","type":"uint256"}],"name":"renewPolicy","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"confirmRenewal","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[],"name":"applyPolicy","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"recordID","type":"uint256"},{"name":"_masterkey","type":"string"}],"name":"getRecordsApplied","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getRecords","outputs":[{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renewPolicy","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"state","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getPremium","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"policyLapse","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"confirmPolicy","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[],"name":"policyInactive","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getDetails","outputs":[{"name":"","type":"address"},{"name":"","type":"address"},{"name":"","type":"uint256"},{"name":"","type":"uint8"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"string"},{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"prevState","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"contractBuyer","type":"address"},{"name":"contractSeller","type":"address"},{"name":"_coverage","type":"uint256"}],"payable":true,"stateMutability":"payable","type":"constructor"}];
 
-const recv = '0xa577C9d2BA3bB4aD1e09df6dFf697Cb3A639eB7a';
+//const recv = '0xa577C9d2BA3bB4aD1e09df6dFf697Cb3A639eB7a';
 
 const Agenda = require('agenda');
 
@@ -46,7 +46,7 @@ async function getTransactionCount(data){
   // return count;
 }
 
-async function run() {
+module.exports = async(org_address, contract_address) => {
   //const db = await MongoClient.connect('mongodb://localhost:27017/agendatest');
 
   // Agenda will use the given mongodb connection to persist data, so jobs
@@ -60,7 +60,7 @@ async function run() {
     await getTransactionCount(job.attrs.data, () => {
       console.log("done")
     })
-    process.exit(0);
+    return true;
   });
 
   // Wait for agenda to connect. Should never fail since connection failures
@@ -70,11 +70,12 @@ async function run() {
   // Schedule a job for 1 second from now and persist it to mongodb.
   // Jobs are uniquely defined by their name, in this case "hello"
 
-  agenda.schedule(new Date(Date.now() + 20000), 'transact',{contract : recv, worker : user});
+  agenda.schedule(new Date(Date.now() + 20000), 'transact',{contract : contract_address, worker : '0x917F5a04E325317960a645F967041ED4C25cc918'});
   agenda.start();
+  return true;
 }
 
-run().catch(error => {
-  console.error(error);
-  process.exit(-1);
-});
+// run().catch(error => {
+//   console.error(error);
+//   process.exit(-1);
+// });
